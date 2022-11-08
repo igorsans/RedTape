7898e7290c5805959884bc89be805be26ed1b34d
igorsans <igor.santos0@hotmail.com>
2022-11-08 19:38:26 -0300

    :recycle: feat: banner dinamico

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,14 +2,14 @@ import styled from "styled-components";
 
 const StyledBanner = styled.div`
     width: 100%;
-    height: 230px;
-    background-image: url(https://images.unsplash.com/photo-1459233313842-cd392ee2c388?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80);
+    height: 280px;
+    background-image: url(${({bg}) => bg});
     background-repeat: no-repeat;
     background-size: cover;
     background-position: center center;
 `
-export function Banner() {
+export function Banner({bg}) {
     return (
-        <StyledBanner/>
+        <StyledBanner bg={bg}/>
     )
 }
