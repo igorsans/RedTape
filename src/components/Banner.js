9c239018c202414a051423f752f80478720f61d9
igorsans <igor.santos0@hotmail.com>
2022-11-11 15:05:45 -0300

    :iphone: Responsivo

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,37 +1,72 @@
 import styled from "styled-components";
 
 const StyledBanner = styled.div`
-    width: 100%;
-    height: ${({ videoUp }) => videoUp? "400px" : "300px"};
-    background-image: url(${({bg}) => bg});
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-position: center center;
-    display: flex;
-    justify-content: center;
-    align-items: flex-end;
-`
+  width: 100%;
+  height: ${({ videoUp }) => (videoUp ? "400px" : "300px")};
+  background-image: url(${({ bg }) => bg});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center center;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  @media (max-width: 425px)
+{
+    height: ${({ videoUp }) => (videoUp ? "285px" : "200px")};
+}
+`;
 const StyledVideo = styled.div`
-display: flex;
-align-items: flex-start;
-    button{
-        margin: 10px 5px;
-        border: none;
-        border-radius: 50%;
-        background-color: ${({theme}) => theme.backgroundBase || "#222222" };
-        color: ${({ theme }) => theme.textColorBase || "#222222"} ;
-        width: 15px;
-        height: 15px
-    }
-`
-export function Banner({bg, videoURL, videoUp, changeVideoUp}) {
+  display: flex;
+  align-items: flex-start;
 
-    return (
-        <StyledBanner videoUp={videoUp} bg={bg}>
-            {videoUp? <StyledVideo>
-            <iframe width="650px" height="350px" src={`https://www.youtube.com/embed/${videoURL}`} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; " allowfullscreen="true"></iframe>
-            <button onClick={() => changeVideoUp()}>X</button>
-            </StyledVideo>: ""}
-        </StyledBanner>
-    )
+  iframe {
+    width: 650px;
+    height: 350px;
+  }
+  button {
+    margin: 10px 5px;
+    border: none;
+    border-radius: 50%;
+    background-color: ${({ theme }) => theme.backgroundBase || "#222222"};
+    color: ${({ theme }) => theme.textColorBase || "#222222"};
+    width: 15px;
+    height: 15px;
+  }
+  @media (max-width: 767px)
+{
+    iframe
+   {
+    width: 400px;
+    height: 350px;
+   }
+}
+@media (max-width: 425px)
+{
+    flex-direction: column-reverse;
+    align-items: center;
+    iframe
+   {
+    width: 100%;
+    height: 200px;
+   }
+}
+`;
+export function Banner({ bg, videoURL, videoUp, changeVideoUp }) {
+  return (
+    <StyledBanner videoUp={videoUp} bg={bg}>
+      {videoUp ? (
+        <StyledVideo>
+          <iframe
+            src={`https://www.youtube.com/embed/${videoURL}`}
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; "
+            allowfullscreen="true"
+          ></iframe>
+          <button onClick={() => changeVideoUp()}>X</button>
+        </StyledVideo>
+      ) : (
+        ""
+      )}
+    </StyledBanner>
+  );
 }
